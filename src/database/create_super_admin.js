c90bf1abac45e2b1896cb2999cbb6431c2af9e5c
Yaroslav Mykhalchuk <mykhalchuk.yaroslav@wetelo.com>
2020-12-04 22:13:37 +0200

    fixed seed again

--- src/database/create_super_admin.js
+++ src/database/create_super_admin.js
@@ -4,9 +4,9 @@ const { userStatuses } = require('../constants');
 
 exports.seed = (knex) => knex('users').where('username', 'superadmin').delete();
 
-exports.seed = (knex) => knex('users').insert({ username: 'superadmin',
-  firstname: 'superadmin',
-  lastname: 'superadmin',
+exports.seed = (knex) => knex('users').insert({ username: 'secondsuperadmin',
+  firstname: 'secondsuperadmin',
+  lastname: 'secondsuperadmin',
   email: 'superadmin@gm.com',
   password: bcrypt.hashSync('123123', 10),
   salt: 10,

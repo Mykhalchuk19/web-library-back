520396d41e22b10825cb1a3225b74082150c8646
Yaroslav Mykhalchuk <mykhalchuk.yaroslav@wetelo.com>
2020-12-15 23:31:34 +0200

    change index for deploy

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const { UserRouter, AuthRouter, CategoryRouter, BookRouter, AuthorRouter } = req
 const app = express();
 const knexFile = require('./knexfile');
 // for dev you need to use development, for prod - production
-const knex = Knex(knexFile.development);
+const knex = Knex(knexFile.production);
 Model.knex(knex);
 
 app.use('/apidoc', express.static('apidoc'));
